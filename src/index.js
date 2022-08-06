cafab9d9d579421333fd7f44504c7d6385314115
Alexandru <T_alexandru87@yahoo.com>
2022-08-06 09:42:01 +0100

    set up server continue with routes

---
+++ src/index.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const routes = require("./routes");
+
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+const init = async () => {
+  const DB_NAME = process.env.DB_NAME;
+  const MONGODB_URI = process.env.MONGODB_URI || `mongodb://localhost:27017/${DB_NAME}`;
+
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+  await mongoose.connect(MONGODB_URI, options);
+
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+};
+
+init();
